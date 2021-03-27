8a7ec5a03577ce19e0d8942d8d17b5e0de209699
omwalhekar <omwalhekar159@gmail.com>
2021-03-28 00:09:42 +0530

    make it better2

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const session = require("express-session");
 const passport = require("passport");
 const connectDB = require("./config/connectdb");
 const PORT = process.env.PORT || 5000;
-require("./models/User");
 require("./services/passport");
 
 connectDB();
